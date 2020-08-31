b113451b9230759b018eac8b98241a24b0ed4909
keiusukematsuda <keisuke.matsuda.680@gmail.ocm>
2020-08-31 11:16:32 +0900

    display events refactor & implement remove button

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,15 +1,15 @@
 const events = (state = [], action) => {
     switch(action.type) {
-        case 'CREATE-EVENT':
+        case 'CREATE_EVENT':
             const event = { title:action.title, body: action.body };
             const length = state.length;
             let id = (length === 0 ? 1 : state[length - 1].id + 1 );
             return [...state, {id, ...event}];
-        case 'DELETE-EVENT':
-            return 
-        case 'DELETE-ALL-EVENTS':
+        case 'DELETE_EVENT':
+            return state.filter( event => event.id !== action.id);
+        case 'DELETE_ALL-EVENTS':
             return [];
-                    default:
+        default:
             return state;
     }
 }
